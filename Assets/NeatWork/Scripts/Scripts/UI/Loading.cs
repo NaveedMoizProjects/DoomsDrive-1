24661ab9efe13f6e449745c978ad29c17f80da92
syedmoiz10 <syedmoizahmed466@gmail.com>
2026-03-19 05:25:21 +0500

    moizWork

--- Assets/NeatWork/Scripts/Scripts/UI/Loading.cs
+++ Assets/NeatWork/Scripts/Scripts/UI/Loading.cs
@@ -1,19 +1,40 @@
-
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections; // Required for Coroutines
 
 public class Loading : MonoBehaviour
 {
+    // Changed from int to string
+    public string sceneName;
 
-    public int levelToLoad;
-
-    [SerializeField] private Slider loadingSlidor;
+    [SerializeField] private Slider loadingSlider;
     [SerializeField] private GameObject loadingScreen;
 
     public void loadingAnim()
     {
-        gameObject.transform.parent.parent.gameObject.SetActive(true);
-        SceneManager.LoadScene(levelToLoad);
+        // Activate the loading UI
+        if (loadingScreen != null)
+            loadingScreen.SetActive(true);
+
+        // Start the background loading process
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        // This runs the loading in the background
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!operation.isDone)
+        {
+            // Clamp progress from 0 to 1
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            if (loadingSlider != null)
+                loadingSlider.value = progress;
+
+            yield return null;
+        }
     }
 }
